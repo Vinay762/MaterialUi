2b641e5e3d65be66e0190c3267e92e62091a60c7
Vinay <vinaytiwari1501@gmail.com>
2022-12-22 14:51:36 +0530

    ImageList

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 //import { MuiLayout } from './components/MuiLayout';
 //import { MuiCards } from './components/MuiCards';
-import { MuiAccordian } from './components/MuiAccordian';
+//import { MuiAccordian } from './components/MuiAccordian';
+import { MuiImageList } from './components/MuiImageList';
 
 function App() {
   return (
     <div className="App">
-      <MuiAccordian/>
+      <MuiImageList/>
     </div>
   );
 }
