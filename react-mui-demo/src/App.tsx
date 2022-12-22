5a30d588723a1235aba6e1e3aa115f3439db43ed
Vinay <vinaytiwari1501@gmail.com>
2022-12-22 16:57:07 +0530

    BreadCrumbs

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -4,12 +4,14 @@ import './App.css';
 //import { MuiAccordian } from './components/MuiAccordian';
 //import { MuiImageList } from './components/MuiImageList';
 //import { MuiNavbar } from './components/MuiNavbar';
-import {MuiLink} from './components/MuiLink'
+// import {MuiLink} from './components/MuiLink'
+import { MuiBreadcrumbs } from './components/MuiBreadcrumbs';
+
 
 function App() {
   return (
     <div className="App">
-      <MuiLink />
+      <MuiBreadcrumbs />
     </div>
   );
 }
