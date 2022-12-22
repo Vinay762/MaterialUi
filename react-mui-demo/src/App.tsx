6614d206b910eb009b7c30ce96ba0d49254bfdcc
Vinay <vinaytiwari1501@gmail.com>
2022-12-22 15:34:15 +0530

    NavBar

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -2,12 +2,13 @@ import './App.css';
 //import { MuiLayout } from './components/MuiLayout';
 //import { MuiCards } from './components/MuiCards';
 //import { MuiAccordian } from './components/MuiAccordian';
-import { MuiImageList } from './components/MuiImageList';
+//import { MuiImageList } from './components/MuiImageList';
+import { MuiNavbar } from './components/MuiNavbar';
 
 function App() {
   return (
     <div className="App">
-      <MuiImageList/>
+      <MuiNavbar />
     </div>
   );
 }
